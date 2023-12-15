4324fd2bb95369ad8be79dbef6e4faa8a5bbcbc3
Antonel Gabriel Crișan <antonel19.gabriel@gmail.com>
2023-12-15 09:52:11 +0200

    update

--- src/Individ.java
+++ src/Individ.java
@@ -2,13 +2,58 @@ import java.util.ArrayList;
 import java.util.Random;
 public class Individ {
     public ArrayList<Integer> traseu = new ArrayList<Integer>();
+    public static double haversine(double lat1, double lon1,
+                            double lat2, double lon2)
+    {
+        // distance between latitudes and longitudes
+        double dLat = Math.toRadians(lat2 - lat1);
+        double dLon = Math.toRadians(lon2 - lon1);
+
+        // convert to radians
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        // apply formulae
+        double a = Math.pow(Math.sin(dLat / 2), 2) +
+                Math.pow(Math.sin(dLon / 2), 2) *
+                        Math.cos(lat1) *
+                        Math.cos(lat2);
+        double rad = 6371;
+        double c = 2 * Math.asin(Math.sqrt(a));
+        return rad * c;
+    }
     public void generareTraseu() {
         Random rand = new Random();
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 19; i++) {
             traseu.add(rand.nextInt(20));
         }
     }
     public void getTraseu() {
         System.out.println(traseu);
+    }
+
+    public double getDistance(Harta harta){
+        double distantaTotala = 0, x1, y1, x2, y2;
+        Localitate start = harta.localitati[0];
+
+        x2 = harta.localitati[0].getx();
+        y2 = harta.localitati[0].gety();
+
+        distantaTotala += haversine(start.getx(),start.gety(),x2,y2);
+
+        for(int i = 0; i < 18; i++){
+            x1 = harta.localitati[this.traseu.get(i)].getx();
+            y1 = harta.localitati[this.traseu.get(i)].gety();
+            x2 = harta.localitati[this.traseu.get(i+1)].getx();
+            y2 = harta.localitati[this.traseu.get(i+1)].gety();
+            distantaTotala += haversine(x1,y1,x2,y2);
+        }
+
+        x1 = harta.localitati[this.traseu.get(18)].getx();
+        y1 = harta.localitati[this.traseu.get(18)].gety();
+
+        distantaTotala += haversine(x1,y1, start.getx(), start.gety());
+
+        return distantaTotala;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,16 +5,8 @@ public class Main {
         harta.generareHarta();
         Generatie gen = new Generatie();
         gen.creareGeneratii();
-        double distantaTotala = 0, x1, y1, x2, y2, dx, dy;
-        for(int i = 0; i < 19; i++){
-            x1 = harta.localitati[gen.generatie[0].traseu.get(i)].getx();
-            x2 = harta.localitati[gen.generatie[0].traseu.get(i+1)].getx();
-            y1 = harta.localitati[gen.generatie[0].traseu.get(i)].gety();
-            y2 = harta.localitati[gen.generatie[0].traseu.get(i+1)].gety();
-            dx = x2 - x1;
-            dy = y2 - y1;
-            distantaTotala = Math.sqrt(dx * dx + dy * dy);
+        for(Individ i : gen.generatie){
+            i.getDistance(harta);
         }
-        System.out.println(distantaTotala);
     }
 }
